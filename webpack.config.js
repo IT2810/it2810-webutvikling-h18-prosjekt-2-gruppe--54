46bc5b274085131c429f11ff629f5b13169ee19e
Martin Aarvik <maraarvik@gmail.com>
2018-09-12 15:57:59 +0200

    added tabs

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+var HTMLWebPackPlugin = require('html-webpack-plugin');
+var HTMLWebPackPluginConfig = new HTMLWebPackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'head'
+});
+
+module.exports = {
+    entry: __dirname + '/app/index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output: {
+        filename: 'transformed.js',
+        path: __dirname + '/build'
+    },
+    plugins: [HTMLWebPackPluginConfig]
+};
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
